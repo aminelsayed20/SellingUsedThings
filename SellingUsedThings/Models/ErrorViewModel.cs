2f619e7257c3b6ba0a9ab88b5a60614bf4d4b5d3
Amin Elsayed <aminelsayed202020@gmail.com>
2024-05-10 09:38:14 +0300

    Add project files.

---
+++ SellingUsedThings/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SellingUsedThings.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
